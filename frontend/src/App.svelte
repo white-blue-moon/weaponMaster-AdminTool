4c9da6e542f733e90a033184967aaf7d49e6e182
white-blue-moon <eoiuco@gmail.com>
2025-01-15 19:32:41 +0900

    frontend - svelte-routing 추가,

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+	import { Router, Route } from 'svelte-routing';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Router>
+	<Route />
+</Router>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	/* Svelte 에서 기본 세팅된 공백 값 제거 */
+	:global(body) {
+		padding: 0;
 	}
 </style>
