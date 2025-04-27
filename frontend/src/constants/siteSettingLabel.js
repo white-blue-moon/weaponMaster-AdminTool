310d185821b3d85ad458f333dc859327ca67ed36
white-blue-moon <eoiuco@gmail.com>
2025-04-28 02:02:56 +0900

    캐릭터 배너 버전 제어 추가

--- frontend/src/constants/siteSettingLabel.js
+++ frontend/src/constants/siteSettingLabel.js
@@ -6,7 +6,9 @@ export const SETTING_LABEL = {
     home_news_focus_first_ver:  "홈 > 뉴스 포커스 배너 버전 (#1)",
     home_news_focus_second_ver: "홈 > 뉴스 포커스 배너 버전 (#2)",
 
-    
+    // 캐릭터 배너
+    character_banner_ver: "홈 > 캐릭터 배너 버전",
+
     // 점검페이지 포커스 배너
     inspection_main_focus_ver:  "점검 > 메인 포커스 배너 버전",
 }
