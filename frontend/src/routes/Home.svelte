271fd717ff477391d8b22d7afee13fe1b7ed9d04
white-blue-moon <eoiuco@gmail.com>
2025-05-08 20:23:05 +0900

    페이지 자체 접근 - 홈 화면 전환 효과 추가

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,18 +1,48 @@
 <script>
+    import { onMount } from "svelte";
+    import { useLocation } from "svelte-routing";
+
     import Gnb from "../components/Gnb.svelte";
     import VisualBanner from "../components/VisualBanner.svelte";
     import SettingList from "../components/SettingList.svelte";
     import Footer from "../components/Footer.svelte";
     import Top from "../components/Top.svelte";
+
+    let   show     = false;
+    const location = useLocation();
+
+    onMount(() => {
+        const isFromGate = $location?.state?.fromAccessGate;
+        
+        if (isFromGate) {
+            // 화면이 렌더링될 준비가 되었을 때(show = true) 실행되도록 예약
+            // → 이렇게 하면 화면이 '서서히 등장하는' 애니메이션 효과를 줄 수 있음
+            requestAnimationFrame(() => show = true);
+        }
+
+        if (!isFromGate) {
+            // 즉시 화면을 보여줌 (애니메이션 없이 바로 나타남)
+            show = true;
+        }
+    })
 </script>
 
 
-<Gnb />
-<VisualBanner title="홈페이지 설정 제어"/>
-<SettingList />
-<Footer />
+<div class:fade-in={ show } class="home-wrapper">
+    <Gnb />
+    <VisualBanner title="홈페이지 설정 제어"/>
+    <SettingList />
+    <Footer />
+</div>
 
 
 <style>
+    .home-wrapper {
+		opacity: 0;
+		transition: opacity 2.5s ease-in-out;
+	}
 
+	.fade-in {
+		opacity: 1;
+	}
 </style>
