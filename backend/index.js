7166646c0e01f64ca7176125fe074ded3501a556
white-blue-moon <eoiuco@gmail.com>
2025-05-11 17:22:01 +0900

    cors 제한 조건 추가

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,15 @@ import accessGateAPI  from "./api/access_gate.js"
 const app = express()
 const port = 7770
 
-app.use(cors())         // 다른 도메인에서의 요청을 허용(CORS 정책 적용)
+// 다른 도메인에서의 요청을 허용(CORS 정책 적용)
+// [ 실제 배포 주소, 로컬 어드민 프론트, 로컬 웨펀마스터 프론트 ]
+app.use(cors({
+    origin: [
+      'https://weapon-master-portfolio.duckdns.org',
+      'http://localhost:8880',
+      'http://localhost:8080'
+    ]
+}))         
 app.use(express.json()) // 요청 본문(JSON 형식)을 파싱하여 req.body에 저장
 
 app.use(siteSettingAPI)
